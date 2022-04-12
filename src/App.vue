d7fb781a6dd151680af3f5a2fa8cce161f11a903
Alexandra <belliraxia@gmail.com>
2022-04-12 13:41:19 -0400

    adds signup/login/logout paths

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/signup">Sign Up</router-link> |
+    <router-link to="/login">Log In</router-link> |
+    <router-link to="/logout">Log Out</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
