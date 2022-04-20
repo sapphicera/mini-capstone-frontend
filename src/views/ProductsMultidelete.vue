33e5854111525a5b0b314ab9cd2198f6aa01f16e
Alexandra <belliraxia@gmail.com>
2022-04-20 13:13:24 -0400

    adds start of multidelete

--- src/views/ProductsMultidelete.vue
+++ src/views/ProductsMultidelete.vue
@@ -1,22 +1,43 @@
-<template>
+<!-- <template>
   <div>
     <VueMultiselect v-model="selected" :options="options">
+      :multiple="true"
     </VueMultiselect>
+    selected: {{ selected }}
+    <button v-on:click="deleteProducts()">delete all</button>
   </div>
 </template>
 
 <script>
 import VueMultiselect from 'vue-multiselect'
+import axios from 'axios'
+
 export default {
   components: { VueMultiselect },
+  methods: {
+    deleteProducts: function () {
+      console.log("deleting products...");
+      console.log(this.selected);
+      axios.delete("/products/multidelete.json", { data: { products: } }).then((response) => {
+        console.log(response.data);
+        this.$router.push("/")
+      })
+    }
+  },
+  created: function () {
+    axios.get('/products.json?simple=true').then((response) => {
+      console.log(response.data);
+      this.options = response.data;
+    })
+  },
   data() {
     return {
       selected: null,
-      options: ['list', 'of', 'options']
+      options: []
     }
   }
 }
 </script>
 
 <style src="vue-multiselect/dist/vue-multiselect.css">
-</style>
+</style> -->
